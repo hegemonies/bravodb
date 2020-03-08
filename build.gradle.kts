37dea8f137724f66dfd90b26dc43ef913c61206b
hegemonies <hegemonies@yandex.ru>
2020-03-08 20:31:02 +0700

    [common]: Change kotlin on 1.3.70 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 //    id("org.springframework.boot") version "2.1.12.BUILD-SNAPSHOT"
 //    id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.3.70"
 //    kotlin("plugin.spring") version "1.3.61"
     id("com.google.cloud.tools.jib") version "1.8.0"
 }
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
-    compile("org.jetbrains.kotlinx", "kotlinx-coroutines-reactive", "1.3.3")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-reactive", "1.3.3")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -28,8 +28,8 @@ dependencies {
     implementation("io.rsocket:rsocket-core:1.0.0-RC5")
     implementation("io.rsocket:rsocket-transport-netty:1.0.0-RC5")
 
-    compile("org.apache.logging.log4j", "log4j-api", "2.13.0")
-    compile("org.apache.logging.log4j", "log4j-core", "2.13.0")
+    implementation("org.apache.logging.log4j", "log4j-api", "2.13.0")
+    implementation("org.apache.logging.log4j", "log4j-core", "2.13.0")
     implementation("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.0")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
